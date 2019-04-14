ca447a4ae5be78ba8ee09f4d47facf00a34d6f86
Michael Busby <busbyBAM@gmail.com>
2019-04-14 07:33:26 -0400

    remove background img

--- src/app/users/components/landing-page/landing-page.component.ts
+++ src/app/users/components/landing-page/landing-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'landing-page',
+  selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
