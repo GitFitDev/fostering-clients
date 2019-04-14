8a42f74645c47ecf70407b68b29f7d0fc75258b1
Michael Busby <busbyBAM@gmail.com>
2019-04-14 02:00:15 -0400

    moving material modules to separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './users/components/home/home.component';
 import { CardListComponent } from './users/components/card-list/card-list.component';
@@ -11,26 +13,8 @@ import { SkillsListComponent } from './users/components/skills-list/skills-list.
 import { FABArrowComponent } from './shared/components/fabarrow/fabarrow.component';
 import { AssessmentComponent } from './users/components/assessment/assessment.component';
 import { LandingPageComponent } from './users/components/landing-page/landing-page.component';
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatCardModule
-} from '@angular/material';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatTabsModule } from '@angular/material/tabs';
-import { ChartsModule } from 'ng2-charts';
 import { LoginComponent } from './users/components/login/login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { RegistrationComponent } from './users/components/registration/registration.component';
-import { MatRadioModule } from '@angular/material/radio';
 import { BattleMenuComponent } from './users/components/battle-menu/battle-menu.component';
 
 @NgModule({
@@ -50,24 +34,10 @@ import { BattleMenuComponent } from './users/components/battle-menu/battle-menu.
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatListModule,
-    MatCardModule,
-    MatIconModule,
-    MatStepperModule,
-    MatDialogModule,
-    MatGridListModule,
-    MatDividerModule,
-    MatTabsModule,
     ChartsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatProgressSpinnerModule,
-    MatDatepickerModule,
-    MatRadioModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatRadioModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatRadioModule
+  ]
+})
+export class MaterialModule {}
